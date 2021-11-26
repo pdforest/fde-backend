00845a2191d044c3dfa407e7252f393287d45168
pdforest <pdforest@gmail.com>
2021-11-26 19:00:53 -0300

    Commit inicial

---
+++ src/main/java/com/demo/fde/FdeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.fde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/fde/FdeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.fde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
